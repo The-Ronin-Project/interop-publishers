4a654e3520273d596e163a7491648ffdc24afc73
Susan Korgen <susan@projectronin.com>
2022-01-05 11:48:58 -0500

    Add AidboxClient publish()
    Add publish() method to AidboxClient, add its unit tests to AidboxClientTest, adjust AidboxUtils.
    
    Remove the now-unused AidboxClient.findPatient(), PatientService, PatientServiceTest.

--- interop-aidbox/src/main/kotlin/com/projectronin/interop/aidbox/client/AidboxClient.kt
+++ interop-aidbox/src/main/kotlin/com/projectronin/interop/aidbox/client/AidboxClient.kt
@@ -1,17 +1,20 @@
 package com.projectronin.interop.aidbox.client
 
-import com.projectronin.interop.aidbox.client.model.GraphQLPostRequest
+import com.projectronin.interop.aidbox.utils.respondToException
+import io.ktor.client.features.ClientRequestException
+import io.ktor.client.features.RedirectResponseException
+import io.ktor.client.features.ServerResponseException
 import io.ktor.client.HttpClient
 import io.ktor.client.request.accept
 import io.ktor.client.request.headers
-import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.contentType
+import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 /**
@@ -21,36 +24,52 @@ import org.springframework.stereotype.Component
 class AidboxClient(
     @Qualifier("aidbox")
     private val httpClient: HttpClient,
-    @Value("\${aidbox.graphql.url}") private val aidboxGraphQLUrl: String
+    private val urlRest: String
 ) {
     private val logger = KotlinLogging.logger { }
 
     /**
-     * Submits the given query to Aidbox and returns the resulting response.
-     * @param query The parameterized query that should be run through Aidbox
-     * @param authString The authorization string that should be provided to Aidbox
-     * @param parameters The optional Map of parameters. The key should match the parameter name provided in the query.
-     * @return The HTTP Response. Consumers can then use the [receive] function to extract the parameterized
-     * [com.projectronin.interop.aidbox.model.GraphQLResponse] object. This is required due to issues with Jacoco and inline functions.
+     * Publishes resources to Aidbox via its REST API at its configured base URL.
+     * Sends a PUT / call with the raw JSON String for a collection of raw JSON resources (not a FHIR bundle).
+     * The resources may be any mix of FHIR resource types.
+     * For efficiency, Aidbox does not validate the resources submitted using PUT / or $import or $load.
+     * * PUT / does not require an id to be on any resource, but in practice, we expect to provide id values in this data.
+     * For an existing Aidbox id, PUT / updates that resource with the new data. For a new id, it adds the resource.
+     * @param rawJsonCollection Collection of raw JSON strings that each represent a FHIR resource to publish.
+     * @return The HTTP Response. By design, we do not examine the payload of resources in the 200 from this API call.
      * @throws RedirectResponseException for a 3xx response.
      * @throws ClientRequestException for a 4xx response.
      * @throws ServerResponseException for a 5xx response.
      */
-    suspend fun query(query: String, authString: String, parameters: Map<String, String> = mapOf()): HttpResponse {
-        // We're intentionally not logging the parameters here since they could have PHI.
-        logger.debug { "Processing Aidbox query: $query" }
+    suspend fun publish(rawJsonCollection: String, authString: String): HttpResponse {
+        val truncate = rawJsonCollection.indexOf('}') + 1
+        val showArray = rawJsonCollection.substring(0, truncate).replace("\\s".toRegex(),"")
+        logger.debug { "Aidbox publish of:\n$showArray ..." }
 
-        val response: HttpResponse = httpClient.post(aidboxGraphQLUrl) {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $authString")
+        val response: HttpResponse = runBlocking {
+            try {
+                httpClient.put("$urlRest/") {
+                    headers {
+                        append(HttpHeaders.Authorization, "Bearer $authString")
+                    }
+                    contentType(ContentType.Application.Json)
+                    accept(ContentType.Application.Json)
+                    body = rawJsonCollection
+                }
+            } catch (e: Exception) {
+                respondToException<HttpResponse>(e)
             }
-            contentType(ContentType.Application.Json)
-            accept(ContentType.Application.Json)
-
-            body = GraphQLPostRequest(query = query, variables = parameters.toSortedMap())
         }
 
-        logger.debug { "Aidbox query returned ${response.status}" }
+        val message = "Aidbox publish returned ${response.status}"
+        logger.debug { message }
+
+        val status = response.status.toString().substring(0, 1)
+        when(status) {
+            "3" -> throw RedirectResponseException(response, message)
+            "4" -> throw ClientRequestException(response, message)
+            "5" -> throw ServerResponseException(response, message)
+        }
 
         return response
     }
--- interop-aidbox/src/test/kotlin/com/projectronin/interop/aidbox/client/AidboxClientTest.kt
+++ interop-aidbox/src/test/kotlin/com/projectronin/interop/aidbox/client/AidboxClientTest.kt
@@ -1,203 +1,124 @@
 package com.projectronin.interop.aidbox.client
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.projectronin.interop.aidbox.client.model.GraphQLResponse
-import com.projectronin.interop.aidbox.model.TestData
-import io.ktor.client.HttpClient
 import io.ktor.client.call.receive
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respond
 import io.ktor.client.engine.mock.toByteArray
 import io.ktor.client.features.ClientRequestException
-import io.ktor.client.features.json.JacksonSerializer
-import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.RedirectResponseException
+import io.ktor.client.features.ServerResponseException
+import io.ktor.client.HttpClient
+import io.ktor.http.headersOf
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
-import io.ktor.http.headersOf
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
 class AidboxClientTest {
-    private val testUrl = "http://localhost/test/graphql"
+    private val urlRest = "http://localhost/8888"
     private val authString = "Auth-String"
+    private val practitionerList = this::class.java.getResource("/json/AidboxPractitionerList.json")!!.readText()
 
     @Test
-    fun `request with no parameters`() {
-        val query = """
-            |query Test {
-            |   test {
-            |       id,
-            |       name
-            |   }
-            |}
-        """.trimMargin()
-        val expectedBody =
-            """{"query":"query Test {\n   test {\n       id,\n       name\n   }\n}","operationName":null,"variables":{}}"""
-        val expectedResponseJSON = """
-            |{
-            |   "data": {
-            |       "test": [
-            |           {
-            |               "id": "id1",
-            |               "name": "name 1"
-            |           },
-            |           {
-            |               "id": "id2",
-            |               "name": "name 2"
-            |           }
-            |       ]
-            |   }
-            |}
-            """.trimMargin()
-
-        val aidboxClient = createClient(expectedBody, expectedResponseJSON)
-
-        val response = runBlocking {
-            val httpResponse = aidboxClient.query(query, authString)
-            httpResponse.receive<GraphQLResponse<TestDataContainer>>()
-        }
-
-        val expectedData1 = TestData("id1", "name 1")
-        val expectedData2 = TestData("id2", "name 2")
-        val expectedResponse: GraphQLResponse<TestDataContainer> =
-            GraphQLResponse(data = TestDataContainer(listOf(expectedData1, expectedData2)))
-        assertEquals(expectedResponse, response)
+    fun `confirm reading from test resources json files`() {
+        val duplicateList = this::class.java.getResource("/json/AidboxPractitionerList.json")!!.readText()
+        assert(duplicateList.length == 3069)
+        assertEquals(practitionerList, duplicateList)
     }
 
     @Test
-    fun `request with parameterized query and single variable`() {
-        val query = """
-            |query Test(${"$"}name: String!) {
-            |   test(name: ${"$"}name) {
-            |       id,
-            |       name
-            |   }
-            |}
-        """.trimMargin()
-        val expectedBody =
-            """{"query":"query Test(${"$"}name: String!) {\n   test(name: ${"$"}name) {\n       id,\n       name\n   }\n}","operationName":null,"variables":{"name":"name 1"}}"""
-        val expectedResponseJSON = """
-            |{
-            |   "data": {
-            |       "test": [
-            |           {
-            |               "id": "id1",
-            |               "name": "name 1"
-            |           }
-            |       ]
-            |   }
-            |}
-            """.trimMargin()
+    fun `publish array of 3 Practitioner resources to aidbox, response 2xx`() {
+        val urlPublish = "$urlRest/"
+        val expectedBody = practitionerList
+        val expectedResponseJSON = practitionerList
+        val expectedResponseStatus = HttpStatusCode.OK
 
-        val aidboxClient = createClient(expectedBody, expectedResponseJSON)
+        val aidboxClient = createClient(expectedBody, expectedResponseJSON, urlPublish)
+        var actualResponseJSON: String
+        var actualResponseStatus: HttpStatusCode
 
-        val response = runBlocking {
-            val httpResponse = aidboxClient.query(query, authString, parameters = mapOf("name" to "name 1"))
-            httpResponse.receive<GraphQLResponse<TestDataContainer>>()
+        actualResponseJSON = runBlocking {
+            val httpResponse = aidboxClient.publish(practitionerList, authString)
+            actualResponseStatus = httpResponse.status
+            httpResponse.receive<String>()
         }
-
-        val expectedData1 = TestData("id1", "name 1")
-        val expectedResponse =
-            GraphQLResponse<TestDataContainer>(data = TestDataContainer(listOf(expectedData1)))
-        assertEquals(expectedResponse, response)
+        assertEquals(actualResponseJSON, expectedResponseJSON)
+        assertEquals(actualResponseStatus, expectedResponseStatus)
     }
 
     @Test
-    fun `request with parameterized query and multiple variables`() {
-        val query = """
-            |query Test(${"$"}name: String!, ${"$"}id: String!) {
-            |   test(name: ${"$"}name, id: ${"$"}id) {
-            |       id,
-            |       name
-            |   }
-            |}
-        """.trimMargin()
-        val expectedBody =
-            """{"query":"query Test(${"$"}name: String!, ${"$"}id: String!) {\n   test(name: ${"$"}name, id: ${"$"}id) {\n       id,\n       name\n   }\n}","operationName":null,"variables":{"id":"id1","name":"name 1"}}"""
-        val expectedResponseJSON = """
-            |{
-            |   "data": {
-            |       "test": [
-            |           {
-            |               "id": "id1",
-            |               "name": "name 1"
-            |           }
-            |       ]
-            |   }
-            |}
-            """.trimMargin()
-
-        val aidboxClient = createClient(expectedBody, expectedResponseJSON)
-
-        val response = runBlocking {
-            val httpResponse = aidboxClient.query(
-                query,
-                authString,
-                parameters = mapOf("name" to "name 1", "id" to "id1")
-            )
-            httpResponse.receive<GraphQLResponse<TestDataContainer>>()
-        }
-
-        val expectedData1 = TestData("id1", "name 1")
-        val expectedResponse =
-            GraphQLResponse<TestDataContainer>(data = TestDataContainer(listOf(expectedData1)))
-        assertEquals(expectedResponse, response)
-    }
-
-    @Test
-    fun `request returns non-200`() {
-        val exception = assertThrows(ClientRequestException::class.java) {
-            val query = """
-            |query Test {
-            |   test {
-            |       id,
-            |       name
-            |   }
-            |}
-        """.trimMargin()
-            val expectedBody =
-                """{"query":"query Test {\n   test {\n       id,\n       name\n   }\n}","operationName":null,"variables":{}}"""
-
-            val aidboxClient = createClient(expectedBody, "", status = HttpStatusCode.Unauthorized)
+    fun `publish array of 3 Practitioner resources to aidbox, response 3xx`() {
+        val expectedResponseStatus = HttpStatusCode.TemporaryRedirect
+        val exception = assertThrows(RedirectResponseException::class.java) {
+            val urlPublish = "$urlRest/"
+            val expectedBody = practitionerList
+            val expectedResponseJSON = ""
+            val aidboxClient = createClient(expectedBody, expectedResponseJSON, urlPublish, responseStatus = expectedResponseStatus)
 
             runBlocking {
-                aidboxClient.query(query, authString)
+                aidboxClient.publish(practitionerList, authString)
             }
         }
+        assertEquals(expectedResponseStatus, exception.response.status)
+    }
 
-        assertEquals(HttpStatusCode.Unauthorized, exception.response.status)
+    @Test
+    fun `publish array of 3 Practitioner resources to aidbox, response 4xx`() {
+        val expectedResponseStatus = HttpStatusCode.Unauthorized
+        val exception = assertThrows(ClientRequestException::class.java) {
+            val urlPublish = "$urlRest/"
+            val expectedBody = practitionerList
+            val expectedResponseJSON = ""
+            val aidboxClient = createClient(expectedBody, expectedResponseJSON, urlPublish, responseStatus = expectedResponseStatus)
+
+            runBlocking {
+                aidboxClient.publish(practitionerList, authString)
+            }
+        }
+        assertEquals(expectedResponseStatus, exception.response.status)
+    }
+
+    @Test
+    fun `publish array of 3 Practitioner resources to aidbox, response 5xx`() {
+        val expectedResponseStatus = HttpStatusCode.ServiceUnavailable
+        val exception = assertThrows(ServerResponseException::class.java) {
+            val urlPublish = "$urlRest/"
+            val expectedBody = practitionerList
+            val expectedResponseJSON = ""
+            val aidboxClient = createClient(expectedBody, expectedResponseJSON, urlPublish, responseStatus = expectedResponseStatus)
+
+            runBlocking {
+                aidboxClient.publish(practitionerList, authString)
+            }
+        }
+        assertEquals(expectedResponseStatus, exception.response.status)
     }
 
     private fun createClient(
         expectedBody: String,
-        response: String,
-        url: String = testUrl,
+        responseContent: String,
+        expectedUrl: String,
+        baseUrl: String = urlRest,
         expectedAuthHeader: String = "Bearer $authString",
-        status: HttpStatusCode = HttpStatusCode.OK
+        responseStatus: HttpStatusCode = HttpStatusCode.OK
     ): AidboxClient {
         val mockEngine = MockEngine { request ->
-            assertEquals(url, request.url.toString())
+            assertEquals(expectedUrl, request.url.toString())
+            assert(expectedUrl.startsWith(baseUrl, ignoreCase = true))
             assertEquals(expectedBody, String(request.body.toByteArray()))
             assertEquals(expectedAuthHeader, request.headers["Authorization"])
 
             respond(
-                content = response,
-                status = status,
+                content = responseContent,
+                status = responseStatus,
                 headers = headersOf(HttpHeaders.ContentType, "application/json")
             )
         }
-
         val httpClient = HttpClient(mockEngine) {
-            install(JsonFeature) {
-                serializer = JacksonSerializer() {
-                    disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                }
-            }
         }
-        return AidboxClient(httpClient, url)
+
+        return AidboxClient(httpClient, baseUrl)
     }
 }
-
-data class TestDataContainer(val test: List<TestData>)
