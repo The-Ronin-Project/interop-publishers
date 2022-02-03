8ef1d193be8b7ca0815a2cd79a95a47b743c7278
Josh Smith <43484632+arssycro@users.noreply.github.com>
2022-02-03 14:39:33 -0600

    INT-334: Update interop-fhir groupId (#10)

--- interop-aidbox/interop-aidbox.gradle.kts
+++ interop-aidbox/interop-aidbox.gradle.kts
@@ -7,9 +7,8 @@ plugins {
 
 dependencies {
     implementation("com.projectronin.interop:interop-common:${project.property("interopCommonVersion")}")
-    implementation("com.projectronin.interop.ehr:interop-fhir:${project.property("interopEhrVersion")}")
+    implementation("com.projectronin.interop.fhir:interop-fhir:${project.property("interopFhirVersion")}")
 
     testImplementation("com.projectronin.interop.ehr:interop-ehr:${project.property("interopEhrVersion")}")
-    testImplementation("com.projectronin.interop.ehr:interop-fhir:${project.property("interopEhrVersion")}")
     testImplementation("com.projectronin.interop:interop-common-jackson:${project.property("interopCommonVersion")}")
 }
