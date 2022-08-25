45abba3cdafcd810520c217d650e830327547fa9
Susan Korgen <susan@projectronin.com>
2022-08-25 15:08:55 -0400

    INT-164 - adjust path format to DP needs, publish API JSON (#77)
    * adjust path format to DP needs, publish API JSON
    
    * no need for parameter names in API folder label

--- interop-datalake/src/main/kotlin/com/projectronin/interop/datalake/PublishService.kt
+++ interop-datalake/src/main/kotlin/com/projectronin/interop/datalake/PublishService.kt
@@ -6,6 +6,7 @@ import com.projectronin.interop.fhir.FHIRResource
 import mu.KotlinLogging
 import org.springframework.stereotype.Service
 import java.time.LocalDate
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 /**
@@ -16,30 +17,88 @@ class PublishService(private val azureClient: AzureClient) {
     private val logger = KotlinLogging.logger { }
 
     /**
-     * Publishes FHIR resources to the Azure datalake, each resource is published to a file with the container:
-     * "/fhir-r4/{resourceType}/date={today's date}/tenant_id={tenantId}/{resourceId}.json"
+     * Publishes serialized FHIR R4 resources to the Azure datalake.
+     *
+     * Gets the resourceType and resourceId from the FHIR R4 resource content.
+     * A resourceType is a name defined by the FHIR spec. Examples: Patient, Practitioner, Condition, Observation, etc.
+     * A resourceId could be any valid FHIR id.
+     *
+     * Each resource in [resources] is published to a distinct file in the datalake container.
+     * The file path supports Data Platform needs for code optimization and bronze directory layout:
+     * "/fhir-r4/tenant_id={tenantId}/date={today's date}/resource_type={resourceType}/{resourceId}.json"
      *
      * @param tenantId The tenant mnemonic for the specific resource
-     * @param resourceCollection List of FHIR resources to publish. May be a mixed List with different resourceTypes,
-     * but expects all of them to have a defined ID
-     * @return true for success
+     * @param resources List of FHIR resources to publish. May be a mixed List with different resourceTypes,
+     *                  but expects all of them to have a defined ID
+     * @return true for success; success may include no data to publish
      */
-    fun publish(tenantId: String, resourceCollection: List<FHIRResource>): Boolean {
-        logger.info { "Publishing Ronin clinical data to datalake" }
-        if (resourceCollection.isEmpty()) {
-            logger.debug { "Publishing no data because the collection is empty" }
+    fun publishFHIRR4(tenantId: String, resources: List<FHIRResource>): Boolean {
+        val root = "/fhir-r4"
+        logger.info { "Publishing Ronin clinical data to datalake at $root" }
+        if (resources.isEmpty()) {
+            logger.debug { "Publishing nothing to datalake because the supplied data is empty" }
             return true
         }
         val dateOfExport = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
-        resourceCollection.forEach {
-            val resourceType = it.resourceType.lowercase()
+        resources.forEach {
+            val resourceType = it.resourceType
             val resourceId = it.id?.value
-                ?: throw IllegalStateException("Attempted to publish a resource of type ${it.resourceType} without a FHIR ID for tenant $tenantId")
-            val filePathString = "/fhir-r4/$resourceType/date=$dateOfExport/tenant_id=$tenantId/$resourceId.json"
+                ?: throw IllegalStateException(
+                    "Attempted to publish a ${it.resourceType} resource without a FHIR ID for tenant $tenantId"
+                )
+            val filePathString = "$root/date=$dateOfExport/tenant_id=$tenantId/resource_type=$resourceType/$resourceId.json"
             logger.debug { "Publishing Ronin clinical data to $filePathString" }
             val serialized = JacksonManager.objectMapper.writeValueAsString(it)
             azureClient.upload(filePathString, serialized)
         }
+        return true
+    }
+
+    /**
+     * Publishes serialized JSON data to the Azure datalake.
+     * The [data] is expected to be the response payload from an API call whose response content format is JSON.
+     *
+     * For JSON response data from FHIR APIs, use publishFHIRR4().
+     * For JSON response data from all other APIs, use publishAPIJSON().
+     *
+     * publishAPIJSON() publishes the JSON data to a file in the datalake container.
+     * The file path supports Data Platform needs for code optimization and bronze directory layout:
+     * "/api-json/schema={schema}/tenant_id={tenantId}/date={today’s date}/{millisecond timestamp}.json"
+     *
+     * The data schema is defined by the API, so this method requires input of the [method] and [url]
+     * to uniquely identify the API call. When writing the data, publishAPIJSON() joins these input values
+     * with a hyphen to generate a label for the schema segment of the file path.
+     *
+     * The file name in the path replaces punctuation in the millisecond timestamp with hyphens.
+     *
+     * @param tenantId The tenant mnemonic
+     * @param data Serialized JSON response data from an API call
+     * @param method Method for the API call, example: GET, POST
+     * @param url URL for the API call
+     * @return true for success; success may include no data to publish
+     */
+    fun publishAPIJSON(tenantId: String, data: String, method: String, url: String): Boolean {
+        val root = "/api-json"
+        logger.info { "Publishing Ronin clinical data to datalake at $root" }
+        if (method.isEmpty() || url.isEmpty()) {
+            throw IllegalStateException(
+                "Attempted to publish JSON data from an API response without identifying the API request for tenant $tenantId"
+            )
+        }
+        if (data.isEmpty()) {
+            logger.debug { "Publishing nothing to datalake because the supplied data is empty" }
+            return true
+        }
+        val pathCleanup: Regex = "[^A-Za-z0-9_-]+".toRegex()
+        // Note: schema contributes to a file path limit of 1024 in the bronze directory. No length issues are expected.
+        val schema = "$method-$url"
+            .replace(pathCleanup, "")
+        val dateOfExport = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
+        val timeOfExport = LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME)
+            .replace(pathCleanup, "-")
+        val filePathString = "$root/schema=$schema/date=$dateOfExport/tenant_id=$tenantId/$timeOfExport.json"
+        logger.debug { "Publishing Ronin clinical data to $filePathString" }
+        azureClient.upload(filePathString, data)
         return true
     }
 }
--- interop-datalake/src/test/kotlin/com/projectronin/interop/datalake/PublishServiceTest.kt
+++ interop-datalake/src/test/kotlin/com/projectronin/interop/datalake/PublishServiceTest.kt
@@ -13,10 +13,13 @@ import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import io.mockk.verify
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class PublishServiceTest {
     private val mockClient = mockk<AzureClient> {}
@@ -24,13 +27,13 @@ class PublishServiceTest {
     private val tenantId = "mockTenant"
 
     @Test
-    fun `empty collection processes`() {
-        assertTrue(service.publish(tenantId, emptyList()))
+    fun `empty FHIR R4 collection processes`() {
+        assertTrue(service.publishFHIRR4(tenantId, emptyList()))
         verify(exactly = 0) { mockClient.upload(any(), any()) }
     }
 
     @Test
-    fun `real collection processes`() {
+    fun `real FHIR R4 collection processes`() {
         mockkConstructor(LocalDate::class)
         mockkStatic(LocalDate::class)
 
@@ -47,24 +50,102 @@ class PublishServiceTest {
             id = Id("def"),
             name = "Location2"
         )
-        val practiconer = Practitioner(
+        val practitioner = Practitioner(
             id = Id("abc"),
         )
-        val filePathString = "/fhir-r4/__RESOURCETYPE__/date=1990-01-03/tenant_id=mockTenant/__FHIRID__.json"
-        val locationFilePathString = filePathString.replace("__RESOURCETYPE__", "location")
-        val practitionerFilePathString = filePathString.replace("__RESOURCETYPE__", "practitioner")
+        val filePathString = "/fhir-r4/date=1990-01-03/tenant_id=mockTenant/resource_type=__RESOURCETYPE__/__FHIRID__.json"
+        val locationFilePathString = filePathString.replace("__RESOURCETYPE__", "Location")
+        val practitionerFilePathString = filePathString.replace("__RESOURCETYPE__", "Practitioner")
         val objectMapper = JacksonManager.objectMapper
         justRun { mockClient.upload(locationFilePathString.replace("__FHIRID__", "abc"), objectMapper.writeValueAsString(location1)) }
         justRun { mockClient.upload(locationFilePathString.replace("__FHIRID__", "def"), objectMapper.writeValueAsString(location2)) }
-        justRun { mockClient.upload(practitionerFilePathString.replace("__FHIRID__", "abc"), objectMapper.writeValueAsString(practiconer)) }
-        assertTrue(service.publish(tenantId, listOf(location1, location2, practiconer)))
+        justRun { mockClient.upload(practitionerFilePathString.replace("__FHIRID__", "abc"), objectMapper.writeValueAsString(practitioner)) }
+        assertTrue(service.publishFHIRR4(tenantId, listOf(location1, location2, practitioner)))
         verify(exactly = 3) { mockClient.upload(any(), any()) }
     }
 
     @Test
-    fun `can publish a resource without an ID`() {
+    fun `cannot publish a FHIR R4 resource that has no id`() {
         val badResource = Location()
-        assertThrows<IllegalStateException> { service.publish(tenantId, listOf(badResource)) }
+        val exception = assertThrows<IllegalStateException> {
+            service.publishFHIRR4(tenantId, listOf(badResource))
+        }
+        Assertions.assertEquals(
+            "Attempted to publish a Location resource without a FHIR ID for tenant $tenantId",
+            exception.message
+        )
+    }
+
+    @Test
+    fun `cannot publish a FHIR R4 resource that has a null id`() {
+        val badResource = Location(id = null)
+        val exception = assertThrows<IllegalStateException> {
+            service.publishFHIRR4(tenantId, listOf(badResource))
+        }
+        Assertions.assertEquals(
+            "Attempted to publish a Location resource without a FHIR ID for tenant $tenantId",
+            exception.message
+        )
+    }
+
+    @Test
+    fun `empty API JSON data processes`() {
+        assertTrue(service.publishAPIJSON(tenantId, "", "GET", "/fhir/Appointment"))
+        verify(exactly = 0) { mockClient.upload(any(), any()) }
+    }
+
+    @Test
+    fun `empty API JSON method does not process`() {
+        val exception = assertThrows<IllegalStateException> {
+            service.publishAPIJSON(tenantId, "", "", "/fhir/Appointment")
+        }
+        Assertions.assertEquals(
+            "Attempted to publish JSON data from an API response without identifying the API request for tenant $tenantId",
+            exception.message
+        )
+    }
+
+    @Test
+    fun `empty API JSON url does not process`() {
+        val exception = assertThrows<IllegalStateException> {
+            service.publishAPIJSON(tenantId, "", "GET", "")
+        }
+        Assertions.assertEquals(
+            "Attempted to publish JSON data from an API response without identifying the API request for tenant $tenantId",
+            exception.message
+        )
+    }
+
+    @Test
+    fun `real API JSON processes`() {
+        mockkConstructor(LocalDate::class)
+        mockkStatic(LocalDate::class)
+        val mockkLocalDate = mockk<LocalDate> {
+            every { format(DateTimeFormatter.ISO_LOCAL_DATE) } returns "1990-01-03"
+        }
+        every { LocalDate.now() } returns mockkLocalDate
+
+        mockkConstructor(LocalTime::class)
+        mockkStatic(LocalTime::class)
+        val mockkLocalTime = mockk<LocalTime> {
+            every { format(DateTimeFormatter.ISO_LOCAL_TIME) } returns "06:07:42.999"
+        }
+        every { LocalTime.now() } returns mockkLocalTime
+
+        val data = """
+            {
+                "first": "this",
+                "second": {
+                    "aaa": "that",
+                    "bbb": "other"
+                }
+            }
+        """.trimIndent()
+
+        val filePathString = "/api-json/schema=GET-customAppointmentByPatient/date=1990-01-03/tenant_id=mockTenant/06-07-42-999.json"
+        justRun { mockClient.upload(filePathString, data) }
+        assertTrue(service.publishAPIJSON(tenantId, data, "GET", "/custom/AppointmentByPatient"))
+        verify(exactly = 1) { mockClient.upload(any(), any()) }
     }
 
     @AfterEach
