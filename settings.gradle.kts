414525f6d6e59ce1ee41dfc62520763bdba1959f
Josh Smith <43484632+arssycro@users.noreply.github.com>
2022-10-20 14:54:13 -0500

    INT-1153: Upgrade to gradle plugins 2.1.4 (#123)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,11 +11,11 @@ for (project in rootProject.children) {
 
 pluginManagement {
     plugins {
-        id("com.projectronin.interop.gradle.base") version "2.1.3"
-        id("com.projectronin.interop.gradle.integration") version "2.1.3"
-        id("com.projectronin.interop.gradle.publish") version "2.1.3"
-        id("com.projectronin.interop.gradle.spring") version "2.1.3"
-        id("com.projectronin.interop.gradle.version") version "2.1.3"
+        id("com.projectronin.interop.gradle.base") version "2.1.4"
+        id("com.projectronin.interop.gradle.integration") version "2.1.4"
+        id("com.projectronin.interop.gradle.publish") version "2.1.4"
+        id("com.projectronin.interop.gradle.spring") version "2.1.4"
+        id("com.projectronin.interop.gradle.version") version "2.1.4"
     }
 
     repositories {
